ea9aaa9cdb2adb623c68763fc6948c278f5b7d4b
MAC <horlarmihleykan10@gmail.com>
2025-05-14 15:48:39 +0100

    fix: product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductDto, updateProductDto } from './dto/product.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
-
+  
+  @ApiOperation({ summary: 'Get all products' })
   @Get()
   async allProduct() {
     return await this.productService.allProduct();
   }
-
+  
+  @ApiOperation({ summary: 'Get product by id' })
   @Get(':id')
   async getProuctById(@Param('id') id: string) {
     return this.productService.getProuctById(id);
