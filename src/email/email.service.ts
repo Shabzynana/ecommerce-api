b3b96e0066fd982710862dec5dc38cf2a5139df2
MAC <horlarmihleykan10@gmail.com>
2025-05-24 00:48:23 +0100

    fix: mail config

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -19,9 +19,10 @@ export class EmailService {
 
     private prepMailContent(filePath: string) {
         return AppUtilities.readFile(`${this.basePath}/templates/${filePath}`);
-    }    
+    }
 
     async sendConfirmationEmail(user:User) {
+        console.log(process.env.MAIL_FROM, 'mail_from')
         try{
             const token = await this.tokenService.generateConfirmationToken(user.id)
             const confirmUrl = `${process.env.FRONTEND_URL}/auth/login?token=${token.access_token}`;
